792bd43eb5fa4d1bd52f09b526b5261ab02967b0
Fredstone1 <frederik396@gmail.com>
2018-01-12 14:47:27 +0100

    first function

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import numpy as np
+import random
 
-print("hej frederik")
-print("hej magnus")
+def sfd(a,b):
+    while b != 0:
+        t = b;
+        b = a % b;
+        a = t;
+    return a;
 
+def relativP(phi):
+    fundetPrime = False;
+    while fundetPrime == False:
+        e = random.randint(1,phi);
+        if sfd(phi,e) == 1:
+            fundetPrime = True;
+            return e;
+
+    
