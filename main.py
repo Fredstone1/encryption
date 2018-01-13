a024122088790efed267a7926b67bded66c7ec7f
Magnus Glasdam Jakobsen <Home@Magnuss-MacBook-Pro-3.local>
2018-01-13 10:20:05 +0100

    her

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import numpy as np
 import random
 
-def sfd(a,b):
-    while b != 0:
-        t = b;
-        b = a % b;
-        a = t;
-    return a;
+def sfd(prime1,prime2):
+    while prime2 != 0:
+        temp = prime2;
+        prime2 = prime1 % prime2;
+        prime = temp;
+    return prime;
 
-def relativP(phi):
+
+def phiCalc(prime1,prime2):
+    phi = abs((prime1-1)*(prime2-1))/sfd(prime1-1,prime2-1)
+    return phi;
+
+
+def relativPrime(phi):
     fundetPrime = False;
     while fundetPrime == False:
         e = random.randint(1,phi);
         if sfd(phi,e) == 1:
             fundetPrime = True;
             return e;
-
-    
