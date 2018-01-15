f4a2207ee330e970300ef78d474e558354a8be7f
Magnus Glasdam Jakobsen <Home@Magnuss-MacBook-Pro-3.local>
2018-01-15 14:41:22 +0100

    Finish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-from sympy import nextprime
 import random
 
 def sfd(prime1, prime2):
@@ -10,20 +9,15 @@ def sfd(prime1, prime2):
     return prime1;
 
 def phiCalc(prime1, prime2):
-    phi = abs((prime1 - 1) * (prime2 - 1)) / sfd(prime1 - 1, prime2 - 1)
+    phi = (prime1 - 1) * (prime2 - 1)
     return phi;
-
-# def findPrime():
 
 def relativPrime(phi):
     fundetPrime = False
     while fundetPrime == False:
-       e = random.randint(1,phi)
+       e = random.randint((phi/2)+1,phi)
        if sfd(phi,e) == 1:
            fundetPrime = True
-    #        return e;
-    # e = pyprimes.primes_above(phi/2)
-    #e = 1
     return e;
 
 def EUA(phi, e):
@@ -49,59 +43,25 @@ def check(phi, e, d):
     return d, e;
 
 def encrypt(n, e, plaintext):
-    # cipherText = plaintext**e % n
-    # return cipherText
-    if n == 1:
-        return 0;
-    result = 1
-    base = plaintext % n
-    while e > 0:
-        if e % 2 == 1:
-            result = (result * base) % n
-        e = e >> 1
-        base = (base * base) % n
-    return result
+    ciphertext = pow(plaintext, e, n)
+    return ciphertext;
 
 def decrypt(n, d, ciphertext):
-    # potens = cipherText**d
-    # plainText = potens % n
-    # ------------------------------------------------------------#
-    # Fundet algoritme på nettet, snak med Jacob og brugen af det.
-    if n == 1:
-        return 0;
-    result = 1
-    base = ciphertext % n
-    while d > 0:
-        if d % 2 == 1:
-            result = (result * base) % n
-        d = d >> 1
-        base = (base * base) % n
-    return result
+    plaintext = pow(ciphertext, d, n)
+    return plaintext;
 
 # ------------------------------------------------------------#
-p = 1272833
-q = 1271971
-n = int(p * q)
+p = 5915587277
+q = 1500450271
 # ------------------------------------------------------------#
-# Du skal ikke slette det, det er nice og nemt at holde styr på
-print('n = ', n, ' nLength = ', len(str(int(n))))
+n = int(p * q)
 phi = int(phiCalc(p, q))
-print('phi = ', phi, ' phiLength = ', len(str(int(phi))), ' phiHalfed = ', phi / 2)
-e = nextprime(phi/2)
-print('e = ', e)
-# e = relativPrime(phi)
-#d = pow(phi, e, n)
+e = relativPrime(phi)
 d = EUA(phi,e)
 d, e = check(phi, e, d)
-print('d = ', d, 'e = ', e)
-
-enc = pow(17,e,n)
-dec = pow(enc,d,n)
+enc = encrypt(n,e,17)
+dec = decrypt(n,d,enc)
 print(dec)
-#print('encryption = ', encrypt(n, e, 17))
-#ciphertext = encrypt(n, e, 17)
-
-#print('decryption = ', decrypt(n, d, ciphertext))
 # ------------------------------------------------------------#
 
 #TODO
