8d8ed0e6a7ce13d08a697fbe33736c9565cff18c
Fredstone1 <frederik396@gmail.com>
2018-01-15 15:23:16 +0100

    dette er en hemmelig besked til magnus

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import math
+from math import floor
 import random
+
+def txt2ascii(txt):
+    converted = [ord(i) for i in txt]
+    return converted;
+
+def ascii2txt(converted):
+    txt = ''.join(chr(i) for i in converted)
+    return txt;
 
 def sfd(prime1, prime2):
     while prime2 != 0:
@@ -26,7 +34,7 @@ def EUA(phi, e):
     r.append(e), s.append(0), t.append(1)
     k = 2
     while r[-1] != 0:
-        q = math.floor(r[k - 2] / r[k - 1])
+        q = floor(r[k - 2] / r[k - 1])
         r.append(r[k - 2] - q * r[k - 1]), s.append(s[k - 2] - q * s[k - 1]), t.append(t[k - 2] - q * t[k - 1])
         k += 1
     d = t[-1] + t[-2]
@@ -43,26 +51,32 @@ def check(phi, e, d):
     return d, e;
 
 def encrypt(n, e, plaintext):
-    ciphertext = pow(plaintext, e, n)
+    ciphertext = []
+    for i in plaintext:
+        ciphertext.append(pow(i, e, n))
     return ciphertext;
 
 def decrypt(n, d, ciphertext):
-    plaintext = pow(ciphertext, d, n)
+    plaintext = []
+    for i in ciphertext:
+        plaintext.append(pow(i, d, n))
     return plaintext;
 
 # ------------------------------------------------------------#
-p = 5915587277
-q = 1500450271
+p = 2367495770217142995264827948666809233066409497699870112003149352380375124855230068487109373226251983
+q = 1814159566819970307982681716822107016038920170504391457462563485198126916735167260215619523429714031
+plaintext = txt2ascii("hemmelig besked 123")
 # ------------------------------------------------------------#
 n = int(p * q)
 phi = int(phiCalc(p, q))
 e = relativPrime(phi)
 d = EUA(phi,e)
 d, e = check(phi, e, d)
-enc = encrypt(n,e,17)
+enc = encrypt(n,e,plaintext)
 dec = decrypt(n,d,enc)
+dec = ascii2txt(dec)
 print(dec)
 # ------------------------------------------------------------#
 
 #TODO
-# gør dette
+# primtal eller billedkodning
